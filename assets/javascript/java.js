e321fe969eb8a7019d405f4233465212a7da211c
Joe Staugaard <joestaugaard@gmail.com>
2019-03-09 11:44:06 -0500

    added giphy

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,32 +1,14 @@
 //------NY TIMES--------------------------------------------//
 
+var headline1 = "";
+
+
 function buildQueryURL() {
     //Query build for NYTimes API.  Parameters commented out are for follow on development
     //after building MVP
 
     var queryURL = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?";
     var queryParams = { "api-key": "76MLUOUhu8ALZ2Y8BCj6gA5pGcglP951" };
-
-    // Grab text the user typed into the search input, add to the queryParams object
-    //   queryParams.q = $("#search-term")
-    //  .val()
-    //  .trim();
- 
-    // var startYear = $("#start-year")
-    //  .val()
-    //  .trim();
-
-    // if (parseInt(startYear)) {
-    //  queryParams.begin_date = startYear + "0101";
-    // }
-
-    //var endYear = $("#end-year")
-    //  .val()
-    //  .trim();
-
-    //if (parseInt(endYear)) {
-    //  queryParams.end_date = endYear + "0101";
-    //}
 
     // Logging the URL so we have access to it for troubleshooting
     console.log("---------------\nURL: " + queryURL + "\n---------------");
@@ -44,15 +26,41 @@ function getArticles() {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        headline1 = response.results[0].title;
+        console.log("Headline 1 :"+headline1);
+        getGiphys();
     })
+
 }
 
-//Call getArticles when page loads
+//--------GIPHY--------------------------------------//
+
+function getGiphys() {
+  // Build query
+  //var animalToGet = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=HeHkAiN21cAfgDt2c1HZzCPq0GE9yKyj&limit=10&offset=0&rating=G&lang=en&q=" + headline1;
+    console.log("In Giphy Headline 1 : " + headline1);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (gifs) {
+
+    // Loop through the gifs
+    for (var i = 0; i < gifs.data.length; i++) {
+
+      var imgURL = gifs.data[i].images.fixed_height.url;
+      console.log(imgURL);
+
+    } // end loop through array of gifs
+})
+
+} // end getGiphys
+
+//Call getArticles and Giphys when page loads----------??
 $(document).ready(function () {
     console.log("ready");
     getArticles();
+
+
 })
-
-
-
-
